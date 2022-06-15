a2cd0b89f0f6b015fded139ffc4edb73334b6672
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-15 13:29:18 -0300

    feat: create Signup page route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' />
+      <Route path='/'  />
+      <Route path='/signup' element={<Signup />} />
     </Routes>
   );
 }
