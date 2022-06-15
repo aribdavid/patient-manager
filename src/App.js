3bd42a95ea41106a9788c8ae8167f5f596648648
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-15 13:20:27 -0300

    created router structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
 import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Routes>
+      <Route path='/' />
+    </Routes>
   );
 }
 
